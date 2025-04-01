a9fa48403aca520d64e13b792f609455679dc9dd
Rubenjme <rubjaicab@gmail.com>
2025-04-01 10:57:58 +0200

    Se le da funcionalidad al botón de Sign Up de forma que al pulsarlo me lleva a una nueva pantalla de registro

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 Builder.load_file("design.kv") # Carga el archivo kv
 
 class LoginScreen(Screen): # Pantalla de inicio de sesión
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen" # Cambia a la pantalla de registro
+
 
 class RootWidget(ScreenManager): # Pantalla principal que contiene todas las pantallas
+    pass
+
+
+class SignUpScreen(Screen):
     pass
 
 class MainApp(App): # Clase principal de la app
