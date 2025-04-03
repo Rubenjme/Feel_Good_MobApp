67e3ea3aaf91779c596394ef4e56f5fe3ca9b3d2
Rubenjme <rubjaicab@gmail.com>
2025-04-03 11:54:17 +0200

    Se añade archivo json parair guardando los usuarios que se registren.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file("design.kv") # Carga el archivo kv
 
@@ -15,8 +17,12 @@ class RootWidget(ScreenManager): # Pantalla principal que contiene todas las pan
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
-        print(uname, pword)
+        with open("users.json") as file:
+            users = json.load(file)
 
+        users[uname] = {"username": uname, "password": pword, 
+                        "created": datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        print(users)
 class MainApp(App): # Clase principal de la app
     def build(self):
         return RootWidget()
